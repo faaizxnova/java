cd168e44f9fcc403c9abf3cbf42478025801a8d4
faaizxnova <faaizxnova@gmail.com>
2026-06-02 20:07:51 +0530

    Added new file .

--- task/2_6_26/pattern.java
+++ task/2_6_26/pattern.java
@@ -1,5 +1,5 @@
 public class pattern {
-    public static void main(String[] arguments) {
+    public static void main(String[] args) {
         int num = 5;
 
         for(int i = 1; i <= num; i ++) {
